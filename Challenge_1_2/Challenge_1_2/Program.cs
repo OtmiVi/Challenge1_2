d090c4336959a5bf2ec7e19418445f5930491b5f
YuraKyziv <37220376+OtmiVi@users.noreply.github.com>
2019-05-23 18:31:10 +0300

    Add files via upload

---
+++ Challenge_1_2/Challenge_1_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=Int16.Parse(Console.ReadLine());
+            
+            int pastSum1 = 0;
+            int pastSum2 = 1;
+            int sum = 0 ;
+            for (int i = 0; i < n-1; i++)
+            {
+                sum = pastSum1 + pastSum2;
+                pastSum2 = pastSum1;
+                pastSum1 =sum;   
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
